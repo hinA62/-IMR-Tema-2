1dcfd5fe417827e6c491c015be31ff887c784d6f
Veronica <veronikacucu24@gmail.com>
2025-10-20 12:23:31 +0300

    t3

---
+++ Assets/HandGrabAnimator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(Animator))]
+public class HandGrabAnimator : MonoBehaviour
+{
+    public XRBaseControllerInteractor interactor;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator.SetBool("IsGrabbing", false);
+        animator = GetComponent<Animator>();
+
+        if (interactor != null)
+        {
+            interactor.selectEntered.AddListener(OnGrab);
+            interactor.selectExited.AddListener(OnRelease);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (interactor != null)
+        {
+            interactor.selectEntered.RemoveListener(OnGrab);
+            interactor.selectExited.RemoveListener(OnRelease);
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        Debug.Log($"{name} → GRAB detected!");
+        animator.SetBool("IsGrabbing", true);
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        Debug.Log($"{name} → RELEASE detected!");
+        animator.SetBool("IsGrabbing", false);
+    }
+
+}
